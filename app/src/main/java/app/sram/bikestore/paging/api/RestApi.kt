9cece7b05d69767cd416713066a6654cbb76e839
Randy <tangdengcheng@gmail.com>
2021-01-30 12:14:29 -0800

    Change API address

--- app/src/main/java/app/sram/bikestore/paging/api/RestApi.kt
+++ app/src/main/java/app/sram/bikestore/paging/api/RestApi.kt
@@ -13,10 +13,18 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RestApi {
-
-    @GET("movie/popular")
+//    https://maps.googleapis.com/maps/api/place/nearbysearch/json?
+//    key=AIzaSyAiPeUFkMSj7_xfnYQY6Bi2bCiuszuU2e4
+//    &location=47.585220,-122.152400
+//    &type=bicycle_store
+//    &radius=100000
+//    &pagetoken=fgdsfgdsrr
+    @GET("maps/api/place/nearbysearch/json")
     fun popularMovieRx(
-        @Query("page") page: Int
+        @Query("page") page: Int,
+        @Query("location") location: String = "47.584420801582866,-122.15182334946458",
+        @Query("radius") radius: String = "50000",
+        @Query("type") type: String = "bicycle_store"
     ): Single<MoviesResponse>
 
     companion object {
