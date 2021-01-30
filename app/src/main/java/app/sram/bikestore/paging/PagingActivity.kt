3324a9209cb181504356293a30fa9c3a1df5830c
Randy <tangdengcheng@gmail.com>
2021-01-30 02:18:34 -0800

    Delete test dependency.

--- app/src/main/java/app/sram/bikestore/paging/PagingActivity.kt
+++ app/src/main/java/app/sram/bikestore/paging/PagingActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import app.sram.bikestore.paging.di.AppConfig
 import com.adrena.commerce.paging3.R
 import com.adrena.commerce.paging3.databinding.ActivityMainBinding
-import javax.inject.Inject
 
 class PagingActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var appConfig: AppConfig
     private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
