62f1a02487bea32b46364a8f3ad07e6e633aef2c
Randy <tangdengcheng@gmail.com>
2021-01-30 02:41:41 -0800

    remove unused code.

--- app/src/main/java/app/sram/bikestore/paging/dao/AppDatabase.kt
+++ app/src/main/java/app/sram/bikestore/paging/dao/AppDatabase.kt
@@ -1,8 +1,6 @@
 package app.sram.bikestore.paging.dao
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import app.sram.bikestore.paging.data.Converters
@@ -16,28 +14,4 @@ import app.sram.bikestore.paging.data.Converters
 abstract class AppDatabase : RoomDatabase() {
     abstract fun moviesRxDao(): BikeStoresDao
     abstract fun movieRemoteKeysRxDao(): RemoteKeysDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getInstance(context: Context): AppDatabase =
-            INSTANCE
-                ?: synchronized(this) {
-                    INSTANCE
-                        ?: buildDatabase(
-                            context
-                        ).also {
-                            INSTANCE = it
-                        }
-                }
-
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                AppDatabase::class.java,
-                "TMDB.db"
-            )
-                .build()
-    }
 }
